cf81fd41390d9a0fadfdc717944b71fc91042380
AfridiAslam <afridiertugrul96@gmail.com>
2024-05-15 14:42:59 +0530

    CLS-5:Added Officers Dtls

---
+++ OfficerDtls.java
@@ -0,0 +1,17 @@
+public class OfficerDtls{
+
+ public void OfficerDate(){
+
+ //Logic Here.
+}
+
+public Void OfficerDivision(){
+
+ //Logic here.
+}
+
+public VOid OfficersEPF(){
+
+ //Logic Here...
+}
+}
