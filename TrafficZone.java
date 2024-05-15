3e2c2c77c14f400b53454d66282e4cfc162aa0b7
AfridiAslam <afridiertugrul96@gmail.com>
2024-05-15 14:33:34 +0530

    CLS-5:Added Module

---
+++ TrafficZone.java
@@ -0,0 +1,29 @@
+public Class TrafficZone{
+
+
+  public void trafficControl(){
+
+ //Logic Here..
+
+System.out.println("Traffic Is not Clear...Take another Route!!!");
+
+}
+
+public void trafficMonitoring(){
+
+//Logic Here...
+
+System.out.println("Traffic Is Under-COntrol");
+
+}
+
+public String trafficStatus(){
+
+//Logic Here.
+
+System.out.println("Traffic Is good in this Area....!");
+
+System.out.println("take this route save time");
+
+}
+}
